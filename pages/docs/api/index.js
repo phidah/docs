945c3c71a354c485203bfbd7f9f3e3a9e4d0abd9
Max Leiter <mleiter@usc.edu>
2020-08-04 16:22:45 -0700

    Remove v1/v2 from API documentation, update CODEOWNERS, remove lib/data/v2, update issue templates

--- pages/docs/api/index.js
+++ pages/docs/api/index.js
@@ -1,4 +1,4 @@
-import ApiDocs from '~/components/references-mdx/api/v2/index.mdx'
+import ApiDocs from '~/components/references-mdx/api/index.mdx'
 import ReferenceLayout from '~/components/layout/reference'
 import { PRODUCT_NAME } from '~/lib/constants'
 
